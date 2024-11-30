f384ca8e2c4a4a35eff6ffa9b2f454f7574c3bc6
ruanlsdn <ruanlsdn@gmail.com>
2024-11-30 16:53:20 -0300

    refactor: add novo campo para identificar quando o usuario está ativo ou inativo

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @ApiProperty()
+    @IsBoolean()
+    @IsNotEmpty()
+    active: boolean;
+}
